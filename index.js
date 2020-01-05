6016e045985cbd9297962d38657beea5e77c331d
Gustavo Herrera <tavoxr23@gmail.com>
2020-01-05 12:45:17 -0600

    validate passwords functions

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ confirmPassword.addEventListener('focusout',validateConfirmPassword);
 email.addEventListener('focusout',validateEmail);
 
 
-
+//Validators
 //----------first Name-------------------
 function validateFirstName(){
     //check if is empty
@@ -33,6 +33,56 @@ function validateFirstName(){
     return true;
 }
 
+function validateLastName(){
+    //check if is empty
+
+    if(checkIfEmpty(lastName)) return;
+
+    //is if it has only letters
+    if(!checkIfOnlyLetters(lastName)) return;
+    return true;
+}
+
+function validatePassword(){
+    //check if is empty
+    if(checkIfEmpty(password)) return;
+
+    //Must of in certain length
+    if(!meetLength(password, 6, 100)) return;
+
+    //check password against our character set
+
+    //1- a
+    //2- a 1
+    //3- A a 1
+    //4- A a 1 @
+    if(!containsCharacters(password, 4)) return;
+
+    return true;
+}
+
+
+function validateConfirmPassword(){
+    if(password.className !== "valid"){
+        setInvalid(confirmPassword, "Password must be valid");
+        return;
+    }
+
+    //If they match 
+
+    if(password.value !== confirmPassword.value){
+        setInvalid(confirmPassword, "Passwords must match");
+        return;
+    }else{
+        setValid(confirmPassword);
+    }
+    return true;
+
+
+}
+
+
+//  Utility functions
 function checkIfEmpty(field){
     if(isEmpty(field.value.trim())){
         //set field invalid
@@ -79,3 +129,57 @@ function checkIfOnlyLetters(field){
         return false;
     }
 }
+
+
+function meetLength(field, minLength, maxLength){
+    if(field.value.length >= minLength && field.value.length <= maxLength){
+        setValid(field);
+        return true;
+    }else if(field.value.length < minLength){
+        setInvalid(field,`${field.name} must be at least ${minLength} characters long`);
+        return false;
+    }else{
+        setInvalid(field,`${field.name} must not be longer than ${maxLength} characters`)
+        return false;
+
+    }
+    }
+
+ 
+
+    function containsCharacters(field, code){
+        let regEx;
+        switch(code){
+            case 1:
+                    //letters
+                    regEx=/(?=.*[a-zA-Z])/;
+                    return matchWithRegEx(regEx,field, "Must contain at least one letter");
+            case 2:
+                    // At least one letter and one number
+                    regEx=/(?=.*\d)(?=.*[a-zA-Z])/;
+                    return matchWithRegEx(regEx,field, "Must contain at least one letter and one number");
+
+            case 3:
+                     // At least one uppercase letter, one lowercase letter and one number
+                     regEx=/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/;
+                     return matchWithRegEx(regEx,field, "Must contain at least one uppercase letter, one lowercase letter and one number");
+                   
+            case 4:
+                       // At least one uppercase letter, one lowercase letter, one number and one special character (symbol)
+                     regEx=/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/;
+                     return matchWithRegEx(regEx,field, "Must contain at least one uppercase letter, one lowercase letter, one number and one special character");
+
+            default:
+                return false;
+        }
+    }
+
+    function matchWithRegEx(regEx, field, message){
+        if(field.value.match(regEx)){
+            setValid(field);
+            return true;
+        }else{
+            setInvalid(field, message);
+            return false;
+        }
+    }
